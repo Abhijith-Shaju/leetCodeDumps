956e4222ff42d1dcc2908a70618198a0b062c63a
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-02-16 19:41:41 +0530

    0101-SYMMETRIC-TREE Stats: Time: 0 ms (100%), Space: 43.9 MB (15.16%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -18,15 +18,17 @@ class Solution {
         if(left == null && right == null) return true;
         if(left == null || right == null) return false;
 
-
         return (left.val == right.val) && equals(left.left, right.right) && equals(left.right, right.left);
     }
 
     public boolean isSymmetric(TreeNode root) {
-        if(root == null || (root.left == null && root.right == null) ) return true;
-        if(root.left != null && root.right != null){
+        if(root == null) 
+        // || (root.left == null && root.right == null) ) 
+        return true;
+
+        // if(root.left != null && root.right != null){
             return equals(root.left, root.right);
-        }
-        return false;
+        // }
+        // return false;
     }
 }
