c49127665447b5723612690175cd15041045af75
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-02-23 14:10:39 +0530

    2121-FIND-IF-PATH-EXISTS-IN-GRAPH Stats: Time: 338 ms (5.65%), Space: 322.2 MB (5.18%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -2,23 +2,15 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
         
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        for(int i = 0; i < n; i++){
-            ans.add(new ArrayList<>());
-        }
-        
         for(int[] i : edges){
             map.putIfAbsent(i[0], new ArrayList<>());
             map.putIfAbsent(i[1], new ArrayList<>());
-            
             
             map.get(i[0]).add(i[1]);
             map.get(i[1]).add(i[0]);
         }
 
         HashSet<Integer> set = new HashSet<>();
-
         return dfs(source, destination, map, set);
     }
 
@@ -32,6 +24,7 @@ class Solution {
 
         for( int i : map.get(curr) ){
             ans = ans | dfs(i, d, map, set);
+            if(ans) return true;
         }
 
         return ans;
