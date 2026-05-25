4d6f58a28e992e88eecfbe4999539be542ea12d0
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-05-25 10:33:07 +0530

    0226-INVERT-BINARY-TREE Stats: Time: 0 ms (100%), Space: 43.1 MB (10.74%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -14,17 +14,17 @@
  * }
  */
 class Solution {
-    void mirror(TreeNode root) {
+    void invert(TreeNode root){
         if(root == null) return;
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
-        
-        mirror(root.left);
-        mirror(root.right);
+
+        invert(root.left);
+        invert(root.right);
     }
     public TreeNode invertTree(TreeNode root) {
-        mirror(root);
+        invert(root);
         return root;
     }
 }
