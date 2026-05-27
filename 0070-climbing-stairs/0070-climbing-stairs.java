1935ae39cab6e1fc215d9d2f9f286fec46218a6f
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-05-27 10:35:50 +0530

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 42 MB (71.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -16,16 +16,34 @@ class Solution {
     // }
 
     //tabulation
+    // public int climbStairs(int n) {        
+    //     if( n <= 2 ) return n;
+
+    //     int[] dp = new int[n+1];
+    //     dp[1] = 1;
+    //     dp[2] = 2;
+    //     for(int i = 3; i <= n; i++){
+    //         dp[i] = dp[i-1] + dp[i-2];
+    //     }
+
+    //     return dp[n];
+    // }
+
+    //direct approach?? faster and better??
     public int climbStairs(int n) {        
         if( n <= 2 ) return n;
 
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
+        // int[] dp = new int[n+1];
+        int temp1 = 1;
+        int temp2 = 2;
+        int temp3 = 0;
+
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            temp3 = temp1 + temp2;
+            temp1 = temp2;
+            temp2 = temp3;
         }
 
-        return dp[n];
+        return temp3;
     }
 }
