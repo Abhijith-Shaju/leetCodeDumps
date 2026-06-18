2ac13cf5e80dc2e133535ef3b9c950448804900c
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-06-18 20:59:31 +0530

    0414-THIRD-MAXIMUM-NUMBER Stats: Time: 8 ms (18.15%), Space: 47.2 MB (6.86%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,17 +1,37 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        int curr = nums[nums.length-1];
-        int count = 1;
+        // int curr = nums[nums.length-1];
+        // int count = 1;
 
-        for(int i = nums.length-2; i >=0; i--){
-            if(nums[i] == curr)continue;
-            curr = nums[i];
-            count++;
-            if(count == 3)break;
+        // for(int i = nums.length-2; i >=0; i--){
+        //     if(nums[i] == curr)continue;
+        //     curr = nums[i];
+        //     count++;
+        //     if(count == 3)break;
+        // }
+
+        // return (count == 3) ? curr : nums[nums.length-1];
+
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        Set<Integer> set = new HashSet<Integer>();
+
+        for(int i : nums){
+            set.add(i);
+        }
+        for(int i : set){
+           maxHeap.offer(i);
         }
 
-        return (count == 3) ? curr : nums[nums.length-1];
+        if(maxHeap.size() < 3){
+            return maxHeap.peek();
+        }
+
+        maxHeap.poll();
+        maxHeap.poll();
+
+        return maxHeap.peek();
     }
 }
