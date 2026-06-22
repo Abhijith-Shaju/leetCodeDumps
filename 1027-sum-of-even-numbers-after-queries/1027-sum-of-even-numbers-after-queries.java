fc18efbf7d6ebe377f48e930e6dae4ecb57beeb2
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-06-22 15:36:44 +0530

    1027-SUM-OF-EVEN-NUMBERS-AFTER-QUERIES Stats: Time: 2336 ms (16.82%), Space: 52.9 MB (14.33%) - LeetHub

--- 1027-sum-of-even-numbers-after-queries/1027-sum-of-even-numbers-after-queries.java
+++ 1027-sum-of-even-numbers-after-queries/1027-sum-of-even-numbers-after-queries.java
@@ -4,12 +4,9 @@ class Solution {
 
         for(int i = 0; i < queries.length; i++){
             int sum = 0;
-            
             nums[queries[i][1]] += queries[i][0];
 
-            for(int num : nums){
-                if( num % 2 == 0) sum += num;
-            }
+            for(int num : nums) if( num % 2 == 0) sum += num;
 
             ans[i] = sum;
         }
