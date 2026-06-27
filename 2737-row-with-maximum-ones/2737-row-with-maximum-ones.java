b5c7c8b55f99b57fbf5dada9a49ab92db4562bf5
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-06-28 01:50:08 +0530

    2737-ROW-WITH-MAXIMUM-ONES Stats: Time: 4 ms (80.08%), Space: 48.6 MB (13.01%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] rowAndMaximumOnes(int[][] mat) {
-        int[][] temp = new int[mat.length][2];
+        int[] temp = new int[mat.length];
 
         for(int i = 0; i < mat.length; i++){
             int count = 0;
@@ -8,16 +8,15 @@ class Solution {
                 if(mat[i][j] == 1) count++;
             }
 
-            temp[i][0] = 1;
-            temp[i][1] = count;
+            temp[i] = count;
         }
 
         int max = 0;
 
         for(int i = 0; i < temp.length; i++){
-            if( temp[i][1] > temp[max][1]) max = i;
+            if( temp[i] > temp[max]) max = i;
         }
 
-        return new int[]{max, temp[max][1]};
+        return new int[]{max, temp[max]};
     }
 }
