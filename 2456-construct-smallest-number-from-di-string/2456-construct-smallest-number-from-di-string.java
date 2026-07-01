8e13f04a59f2634227bb726f2b7807e973654943
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-07-01 12:05:36 +0530

    2456-CONSTRUCT-SMALLEST-NUMBER-FROM-DI-STRING Stats: Time: 1 ms (86.67%), Space: 42.8 MB (63.57%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -1,18 +1,18 @@
 class Solution {
     public String smallestNumber(String pattern) {
         Deque<Integer> stack = new ArrayDeque<>();
-        String ans = "";
-        
+        StringBuilder ans = new StringBuilder("");
+
         for(int i = 0; i <= pattern.length(); i++){
             stack.push(i+1); 
 
             if(i == pattern.length() || pattern.charAt(i) == 'I'){
                 while( !stack.isEmpty() ) {
-                    ans += Integer.toString(stack.peek());
+                    ans.append( Integer.toString(stack.peek()) );
                     stack.pop();
                 }
             }
         }
-        return ans;
+        return ans.toString();
     }
 }
