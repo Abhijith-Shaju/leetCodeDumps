ebd73d88003a8c517b747d5e96ec68d91eeb5bf0
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-07-01 21:28:14 +0530

    0733-FLOOD-FILL Stats: Time: 0 ms (100%), Space: 46.8 MB (61.6%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] != color){
-            helper(image, sr, sc, color, image[sr][sc]);
-        }
+        
+        if( image[sr][sc] != color ) helper(image, sr, sc, color, image[sr][sc]);
         return image;
     }
 
-    void helper(int[][] img, int r, int c, int color, int prev){
-        if(r >= img.length || r < 0 || c >= img[r].length || c < 0 || img[r][c] != prev){
-            return;
-        }
-        int pre = img[r][c];
-        img[r][c] = color;
+    void helper(int[][] arr, int r, int c, int color, int prev){
+        if( r >= arr.length || r < 0 || c >= arr[r].length || c < 0 || arr[r][c] != prev ) return;
 
-        helper(img, r + 1, c, color, pre);
-        helper(img, r - 1, c, color, pre);
-        helper(img, r, c + 1, color, pre);
-        helper(img, r, c - 1, color, pre);
+        arr[r][c] = color;
+
+        helper(arr, r+1, c, color, prev);
+        helper(arr, r-1, c, color, prev);
+        helper(arr, r, c+1, color, prev);
+        helper(arr, r, c-1, color, prev);
     }
 }
