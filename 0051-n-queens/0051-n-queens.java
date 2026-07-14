da1a78d3c4a31bfcae01277cd855338102e82f51
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-07-14 11:47:01 +0530

    0051-N-QUEENS Stats: Time: 9 ms (5.05%), Space: 46.8 MB (28.63%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -7,22 +7,21 @@ class Solution {
             ans.add(temp);
             return ans;
         }
-        for(int i = 0; i < n; i++){
-            String[][] arr = new String[n][n];
-            int num = helper(arr, 0, i, n);
-            arr[0][i] = "Q";
-            
-            placer(arr, 1, i, n, ans, num);
-            
-        }
+        String[][] arr = new String[n][n];
+        placer(arr, 0, 0, n, ans, 0);
+
         return ans;
     }
 
     void placer(String[][] arr, int i, int j, int n, List<List<String>> ans, int num){
         if(num == n){
             List<String> temp = new ArrayList<>();
-            for(String[] s : arr){
-                temp.add(String.join("", s));
+            for(String[] row : arr){
+                StringBuilder sb = new StringBuilder();
+                for(String cell : row) {
+                    sb.append(cell == null ? "." : cell);
+                }
+                temp.add(sb.toString());
             }
             System.out.println(temp);
             ans.add( new ArrayList<>(temp) );
@@ -30,53 +29,31 @@ class Solution {
         }
         if(i < 0 || j < 0 || i >= n || j >= n) return;
 
-        String[][] copy = new String[arr.length][];
-        for (int x = 0; x < arr.length; x++) {
-            copy[x] = arr[x].clone(); 
-        }
-
         for(int x = 0; x < n; x++){
             if( arr[i][x] == null || !arr[i][x].equals(".") ){
-                helper(arr, i, x, n);
+                if( !helper(arr, i, x, n) ) continue;
                 arr[i][x] = "Q";
                 placer(arr, i+1, j, n, ans, num+1);
-
-                for (int k = 0; k < arr.length; k++) {
-                    arr[k] = copy[k].clone(); 
-                }
+                arr[i][x] = null;
             }
         }
-
-
     }
 
-    int helper(String[][] arr, int i, int j, int n){
+    boolean helper(String[][] arr, int i, int j, int n){
         for(int a = 0; a < n; a++){
-            if(arr[i][a] == "Q" || arr[a][j] == "Q")continue;
-            arr[i][a] = ".";
-            arr[a][j] = ".";
-        }
+            if(arr[i][a] == "Q" || arr[a][j] == "Q") return false;        }
         for(int a = i+1, b = j+1; a < n && b < n; a++, b++){
-            if(arr[a][b] == "Q")continue;
-
-            arr[a][b] = ".";
+            if(arr[a][b] == "Q") return false;
         }
         for(int a = i-1, b = j+1; a >= 0 && b < n; a--, b++){
-            if(arr[a][b] == "Q")continue;
-
-            arr[a][b] = ".";
+            if(arr[a][b] == "Q") return false;
         }
         for(int a = i+1, b = j-1; a < n && b >= 0; a++, b--){
-            if(arr[a][b] == "Q")continue;
-
-            arr[a][b] = ".";
+            if(arr[a][b] == "Q") return false;
         }
         for(int a = i-1, b = j-1; a >= 0 && b >= 0; a--, b--){
-            if(arr[a][b] == "Q")continue;
-
-            arr[a][b] = ".";
+            if(arr[a][b] == "Q") return false;
         }
-
-        return 1;
+        return true;
     }
 }
