4d9c290a38afdc560b6cb9d1f92be52e9c457cd3
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-07-14 11:59:56 +0530

    0051-N-QUEENS Stats: Time: 3 ms (41.02%), Space: 46.9 MB (7.64%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -7,19 +7,19 @@ class Solution {
             ans.add(temp);
             return ans;
         }
-        String[][] arr = new String[n][n];
+        char[][] arr = new char[n][n];
         placer(arr, 0, n, ans, 0);
 
         return ans;
     }
 
-    void placer(String[][] arr, int i, int n, List<List<String>> ans, int num){
+    void placer(char[][] arr, int i, int n, List<List<String>> ans, int num){
         if(num == n){
             List<String> temp = new ArrayList<>();
-            for(String[] row : arr){
+            for(char[] row : arr){
                 StringBuilder sb = new StringBuilder();
-                for(String cell : row) {
-                    sb.append(cell == null ? "." : cell);
+                for(char cell : row) {
+                    sb.append(cell == '\u0000' ? '.' : cell);
                 }
                 temp.add(sb.toString());
             }
@@ -29,27 +29,29 @@ class Solution {
         if(i < 0 || i >= n) return;
 
         for(int x = 0; x < n; x++){
-            if( !helper(arr, i, x, n) ) continue;
-            arr[i][x] = "Q";
-            placer(arr, i+1, n, ans, num+1);
-            arr[i][x] = null;
+            if( arr[i][x] == '\u0000' || !(arr[i][x] == '.') ){
+                if( !helper(arr, i, x, n) ) continue;
+                arr[i][x] = 'Q';
+                placer(arr, i+1, n, ans, num+1);
+                arr[i][x] = '\u0000';
+            }
         }
     }
 
-    boolean helper(String[][] arr, int i, int j, int n){
+    boolean helper(char[][] arr, int i, int j, int n){
         for(int a = 0; a < n; a++){
-            if( "Q".equals(arr[i][a]) || "Q".equals(arr[a][j]) ) return false;        }
+            if(arr[i][a] == 'Q' || arr[a][j] == 'Q') return false;        }
         for(int a = i+1, b = j+1; a < n && b < n; a++, b++){
-            if( "Q".equals(arr[a][b]) ) return false;
+            if(arr[a][b] == 'Q') return false;
         }
         for(int a = i-1, b = j+1; a >= 0 && b < n; a--, b++){
-            if( "Q".equals(arr[a][b]) ) return false;
+            if(arr[a][b] == 'Q') return false;
         }
         for(int a = i+1, b = j-1; a < n && b >= 0; a++, b--){
-            if( "Q".equals(arr[a][b]) ) return false;
+            if(arr[a][b] == 'Q') return false;
         }
         for(int a = i-1, b = j-1; a >= 0 && b >= 0; a--, b--){
-            if( "Q".equals(arr[a][b]) ) return false;
+            if(arr[a][b] == 'Q') return false;
         }
         return true;
     }
