e0443fe70676d10e14d4f358802178d1ffbd77a9
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-07-16 12:43:56 +0530

    0650-2-KEYS-KEYBOARD Stats: Time: 66 ms (5.11%), Space: 154.9 MB (5.11%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -2,20 +2,22 @@ class Solution {
     public int minSteps(int n) {
         if(n == 1) return 0;
         int[][] dp = new int[n+1][n+1];
-        for(int i = 0; i < n; i++)Arrays.fill(dp[i], -1);
+        for(int i = 0; i <= n; i++)Arrays.fill(dp[i], -1);
         return 1 + helper("A", "A", n, dp);
     }
 
     int helper(String s, String copy, int n, int[][] dp){
-        if(s.length() == n) return 0;
-        if(s.length() > n) return 10000;
-        if(dp[s.length()][copy.length()] != -1 ) return dp[s.length()][copy.length()];
+        int len1 = s.length();
+        int len2 = copy.length();
+        if(len1 == n) return 0;
+        if(len1 > n) return 10000;
+        if(dp[len1][len2] != -1 ) return dp[len1][len2];
 
         int x = 1 + helper(s+copy, copy, n, dp);
         copy = s;
         int y = 2 + helper(s+copy, copy, n, dp);
 
-        dp[s.length()][copy.length()] = Math.min(x, y);
-        return dp[s.length()][copy.length()];
+        dp[len1][len2] = Math.min(x, y);
+        return dp[len1][len2];
     }
 }
