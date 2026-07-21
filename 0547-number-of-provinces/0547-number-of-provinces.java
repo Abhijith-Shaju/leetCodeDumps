eac33a579915b8af4a80a43cf0a09e177af7a131
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-07-21 11:07:28 +0530

    0547-NUMBER-OF-PROVINCES Stats: Time: 2 ms (42.27%), Space: 47.1 MB (88.38%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,28 @@
 class Solution {
     public int findCircleNum(int[][] arr) {
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        boolean[] visited = new boolean[arr.length];
+        Queue<Integer> q = new LinkedList<>();        
+        int ans = 0;
 
         for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[i].length; j++){
-                if(arr[i][j] == 1){
-                    map.putIfAbsent(i, new ArrayList<>() );
-                    map.get(i).add(j);
-                }
-            }
-        }
-
-        int ans = 0;
-        
-        Queue<Integer> q = new LinkedList<>();
-        HashSet<Integer> set = new HashSet<>();
-
-        while( !map.isEmpty() ){
-            q.offer(map.keySet().iterator().next());
-            set.add(q.peek());
             
-            while( !q.isEmpty() ){
-                int temp = q.poll();
-                for( int i : map.get(temp) ){
-                    if( !set.contains(i) ){
-                        q.offer(i);
-                        set.add(i);
+            if( !visited[i] ){
+                ans++;
+                visited[i] = true;
+
+                q.offer(i);
+                
+                while( !q.isEmpty() ){
+                    int temp = q.poll();
+                    for( int j = 0; j < arr.length; j++){
+                        if(arr[temp][j] == 1 && visited[j] == false){
+                            visited[j] = true;
+                            q.offer(j);
+                        } 
                     }
                 }
-                map.remove(temp);
+                
             }
-            ans++;
         }
 
         return ans;
