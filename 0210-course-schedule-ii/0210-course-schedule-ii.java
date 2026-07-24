c0c966c958bd029cdef15e7794aeb9f63f86a280
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-07-24 10:39:54 +0530

    0210-COURSE-SCHEDULE-II Stats: Time: 6 ms (42.81%), Space: 47.1 MB (25.84%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,42 +1,46 @@
 class Solution {
-    int[] result;
-    int index;
+    public int[] findOrder(int n, int[][] prerequisites) {
+        int[] ans = new int[n];
 
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
+        if(prerequisites.length == 0){
+            for(int i = 0; i < n; i++){
+                ans[ n-1 -i] = i;
+            }
+            return ans;
+        }
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
 
-        for(int i = 0; i < numCourses; i++){
-            adj.add(new ArrayList<Integer>());
+        for(int[] i : prerequisites)adj.get(i[1]).add(i[0]);
+
+        int[] in = new int[n];
+
+        for(int i = 0; i < n; i++)
+            for(int curr : adj.get(i))
+                in[curr]++; 
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++) {
+            if(in[i] == 0) q.offer(i);
         }
 
-        for(int[] pre : prerequisites){
-            adj.get(pre[1]).add(pre[0]);
+        int count = 0;
+
+        while( !q.isEmpty() ){
+            int top = q.poll();
+            ans[count++] = top;
+
+            for(int i : adj.get(top)){
+                in[i]--;
+                if(in[i] == 0) q.offer(i);
+            }
         }
 
-        int[] state = new int[numCourses];
-
-        result = new int[numCourses];
-        index = numCourses - 1;
-
-        for( int i = 0; i < numCourses; i++ ){
-            if( hasCycle(i , adj, state ) ) return new int[0];
+        if(count != n){
+            return new int[]{};
         }
 
-        return result;
-    }
-
-    boolean hasCycle(int curr, List<List<Integer>> adj, int[] state){
-        if(state[curr] == 1) return true;
-        if(state[curr] == 2) return false;
-
-        state[curr] = 1;
-
-        for(int i : adj.get(curr)){
-            if( hasCycle(i, adj, state)) return true;
-        }
-
-        result[index--] = curr;
-        state[curr] = 2;
-        return false;
+        return ans;
     }
 }
