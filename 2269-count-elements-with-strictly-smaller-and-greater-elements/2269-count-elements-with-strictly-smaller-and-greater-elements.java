fe1cbdc32a3a408445a0d3ce0eb233c88fda9060
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-09-23 19:09:14 +0530

    2269-COUNT-ELEMENTS-WITH-STRICTLY-SMALLER-AND-GREATER-ELEMENTS Stats: Time: 0 ms (100%), Space: 43.3 MB (84.3%) - LeetHub

--- 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -1,13 +1,30 @@
 class Solution {
     public int countElements(int[] nums) {
-        Arrays.sort(nums);
-        int i;
-        int j;
+        // Arrays.sort(nums);
+        // int i;
+        // int j;
 
-        for(i = 0; i < nums.length; i++)if(nums[i] != nums[0]) break;
-        for(j = nums.length-1; j >= 0; j--)if(nums[j] != nums[nums.length-1]) break;
+        // for(i = 0; i < nums.length; i++)if(nums[i] != nums[0]) break;
+        // for(j = nums.length-1; j >= 0; j--)if(nums[j] != nums[nums.length-1]) break;
 
-        if( i < j ) return j - i + 1;
-        return i==j ? 1 : 0; 
+        // if( i < j ) return j - i + 1;
+        // return i==j ? 1 : 0; 
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i : nums){
+            min = Math.min(min, i);
+            max = Math.max(max, i);
+        }
+
+        int minC = 0, maxC = 0;
+        for(int i : nums){
+            if(i == min) minC++;
+            if(i == max) maxC++;
+        }
+
+        if(minC + maxC >= nums.length) return 0;
+
+        return nums.length - ( minC + maxC ); 
     }
 }
